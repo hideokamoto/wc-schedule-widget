ffa583a28f355521d4c24f2c7374b92b8ab70232
hideokamoto <hide.okamoto@digitalcube.jp>
2016-05-01 21:33:08 +0900

    init

---
+++ wc-scheduler.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Wc-scheduler
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: wc-scheduler
+ * Domain Path: /languages
+ * @package Wc-scheduler
+ */
